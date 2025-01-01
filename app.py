685153e2027810ddbb84b01a65568cbffe18c231
Vitalitypi <779958365@qq.com>
2025-01-01 13:05:07 +0800

    链接前端

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 from inference import Inference
 
 infer = Inference()
 # instantiate the app
-app = Flask(__name__)
+app = Flask(__name__, static_folder='dist', template_folder='dist')
 app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
-
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    print(infer.inference(idx=0))
-    return jsonify('pong!')
-
+@app.route('/')
+def serve():
+    return send_from_directory(app.template_folder, 'index.html')
+@app.route('/<path:path>')
+def static_proxy(path):
+    try:
+        return send_from_directory(app.static_folder, path)
+    except Exception as e:
+        print(f"Error serving {path}: {e}")
+        return "File not found", 404
 
 if __name__ == '__main__':
     app.run()
--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,32 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
+import * as echarts from 'echarts';
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('main'));
+var option;
+
+option = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'line'
+    }
+  ]
+};
+
+option && myChart.setOption(option);
